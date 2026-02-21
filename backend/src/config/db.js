b8d9845ccb0dc0c3eebc05a4dc872e5076a31580
yabu <yabsraabesha165@gmail.com>
2026-02-21 00:04:19 -0800

    connecting mongdb

---
+++ backend/src/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDb connected: ${conn.connection.host}`);
+
+    } catch (error) {
+        console.error(`mongodb error: ${error.message}`);
+        process.exit(1);
+
+    };
+
+};
+export default connectDB;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(cors())
 app.use(express.json());
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
