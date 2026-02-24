f40ddb4e691bb4169dc135a5c059b723606bdabe
yabu <yabsraabesha165@gmail.com>
2026-02-24 02:20:04 -0800

    updating the server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import { protect } from "./middleware/authMiddleware.js";
+import eventRoutes from "./routes/eventRoutes.js"
 
 dotenv.config();
 
@@ -11,7 +12,9 @@ const app = express();
 
 app.use(cors())
 app.use(express.json());
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/events", eventRoutes);
+
 app.get("/api/test/protected", protect, (req, res) => {
     res.json({ message: "Protected route accessed", user: req.user });
 });
