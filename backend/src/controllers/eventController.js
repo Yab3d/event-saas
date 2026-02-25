9a7730e99c40f460410ef2bf8b4bb85b04bc6d85
yabu <yabsraabesha165@gmail.com>
2026-02-25 02:27:10 -0800

    updating the eventController with the session

--- backend/src/controllers/eventController.js
+++ backend/src/controllers/eventController.js
@@ -1,35 +1,50 @@
+import mongoose from "mongoose";
 import Event from "../models/Event.js";
 import TicketTier from "../models/TicketTier.js";
 
 export const createEvent = async (req, res) => {
+
+    //start the session
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
     try {
-        console.log("Body received:", req.body);
         const { title, description, category, date, location, tiers } = req.body;
 
-        // 1. Create the Event (Status defaults to 'draft')
-        const event = await Event.create({
+        // 2. Create Event (Pass the session)
+        const event = await Event.create([{
             title,
             description,
             category,
             date,
             location,
             organizer: req.user._id
-        });
+        }], { session });
 
-        // 2. Create the Ticket Tiers linked to this event
+        const newEvent = event[0];
+
         if (tiers && tiers.length > 0) {
             const tiersWithEventId = tiers.map(tier => ({
                 ...tier,
-                event: event._id
+                event: newEvent._id
             }));
-            await TicketTier.insertMany(tiersWithEventId);
+            await TicketTier.insertMany(tiersWithEventId, { session });
         }
 
+
+        await session.commitTransaction();
+        session.endSession();
+
         res.status(201).json({
-            message: "Event and Tiers created successfully",
-            event
+            message: "Event and Tiers created safely",
+            event: newEvent
         });
+
     } catch (error) {
-        res.status(500).json({ message: error.message });
+
+        await session.abortTransaction();
+        session.endSession();
+
+        res.status(500).json({ message: "Transaction failed: " + error.message });
     }
 };
