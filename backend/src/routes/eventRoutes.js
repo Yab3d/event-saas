d7bb04c61a31cebd8d3bce256e1a4cb3a855f4da
yabu <yabsraabesha165@gmail.com>
2026-02-26 08:13:08 -0800

    adidin update routes to the eventRoutes

--- backend/src/routes/eventRoutes.js
+++ backend/src/routes/eventRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { createEvent } from "../controllers/eventController.js";
+import { createEvent, updateEvent } from "../controllers/eventController.js";
 import { protect, authorizeRoles } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", protect, authorizeRoles("organizer"), createEvent);
+
+router.put("/:id", protect, authorizeRoles("organizer"), updateEvent)
 
 export default router;
