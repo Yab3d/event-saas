5791a71dc3d87fcc428fc7dfa62dd3cc96bd57bd
yabu <yabsraabesha165@gmail.com>
2026-02-28 22:13:32 -0800

    updating the adminRoutes

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { reviewEvent, getPendingEvents } from "../controllers/adminController.js";
+import { reviewEvent, getPendingEvents, verifyOrganizer } from "../controllers/adminController.js";
 import { protect, authorizeRoles } from "../middleware/authMiddleware.js";
+
 
 const router = express.Router();
 
@@ -8,5 +9,6 @@ router.use(protect, authorizeRoles("admin"));
 
 router.get("/pending", getPendingEvents);
 router.put("/review/:id", reviewEvent);
+router.put("/verify-organizer/:userId", verifyOrganizer)
 
 export default router;
