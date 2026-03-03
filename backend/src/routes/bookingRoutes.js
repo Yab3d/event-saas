04f6d796565290dee9a9b8fb556fa2f609db84b2
yabu <yabsraabesha165@gmail.com>
2026-03-03 10:43:58 -0800

    updating booking routes

--- backend/src/routes/bookingRoutes.js
+++ backend/src/routes/bookingRoutes.js
@@ -1,10 +1,19 @@
 import express from 'express';
-import { createBooking, getMyBookings } from '../controllers/bookingController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { createBooking, getMyBookings, getEventSales, getAllBookings } from '../controllers/bookingController.js';
+import { authorizeRoles, protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
-
+//customer routes
 router.post('/', protect, createBooking);
 router.get('/my-bookings', protect, getMyBookings);
+
+//orgainzer routes
+router.get('/event/:eventId', protect, getEventSales);
+
+
+//Admin routes
+router.get('/all', protect, authorizeRoles('admin'), getAllBookings);
+
+
 
 export default router;
